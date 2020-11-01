8aaa8cd87182c3100b08c2bfabe2ab5544c01ef3
Henriette Wien <h.s.wien@gmx.de>
2020-11-01 16:58:51 +0100

    Teil

--- src/main/java/model/teil.java
+++ src/main/java/model/teil.java
@@ -2,10 +2,12 @@ package model;
 
 import org.hibernate.annotations.Entity;
 
+import javax.persistence.Column;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity
-public class teil {
+public class teil implements Serializable {
     private Integer teilID;
 
     public void setTeilID(Integer teilID) {
@@ -13,7 +15,51 @@ public class teil {
     }
 
     @Id
+    @Column(name = "teilID")
     public Integer getTeilID() {
         return teilID;
+    }
+    @Column(name="Name")
+    public String Name;
+
+    @Column(name="Funktion")
+    public String Funktion;
+
+    @Column(name="Material")
+    public String Material;
+
+    @Column(name="Preis")
+    public Double Preis;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getFunktion() {
+        return Funktion;
+    }
+
+    public void setFunktion(String funktion) {
+        Funktion = funktion;
+    }
+
+    public String getMaterial() {
+        return Material;
+    }
+
+    public void setMaterial(String material) {
+        Material = material;
+    }
+
+    public Double getPreis() {
+        return Preis;
+    }
+
+    public void setPreis(Double preis) {
+        Preis = preis;
     }
 }
