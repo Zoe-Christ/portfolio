01271d59fc273d3a1664922d7acb0fc5054259d6
Zoe Christ <zoe@christ.de>
2020-11-06 10:40:11 +0100

    sql statements

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -17,17 +17,47 @@ public class Demo {
     public static void main( String[] args ) throws IllegalAccessException, SQLException {
         ConnectionHelper c = new ConnectionHelper();
         Connection con = c.getConnection("portfolio");
+        Statement stmt;
 
-        Statement stmt = con.createStatement();
-        try ( ResultSet rs = stmt.executeQuery( "SELECT * FROM Lager" ) ) {
+        /*Inner Join: zeigt Name des Lieferdienst mit dazugehöriger Vertragslaufzeit*/
+        stmt = con.createStatement();
+        try ( ResultSet rs = stmt.executeQuery( "SELECT Lieferdienst.Name, Lieferdienst.vertrag_Vertrag_ID, Vertrag.Vertrag_ID, Vertrag.Laufzeit from portfolio.Lieferdienst \n" +
+                "inner Join portfolio.Vertrag where Lieferdienst.vertrag_Vertrag_ID = Vertrag.Vertrag_ID" ) ) {
                         while ( rs.next() ) {
-                            System.out.printf( "%s, %s%n", rs.getString( 2 ), rs.getInt( 1 ) );
+                            System.out.printf(rs.getString( 1 ), rs.getInt( 2 ),rs.getInt(3), rs.getString(4) );
                         }
 
         } catch ( SQLException e ) {
             System.err.println( "Error during Demo!" );
             e.printStackTrace();
         }
+        stmt.close();
+
+        /*Sortiert die Filialen nach Umsatz*/
+        stmt = con.createStatement();
+        try ( ResultSet rs = stmt.executeQuery( "SELECT * from portfolio.Filiale order by Umsatz DESC" ) ) {
+            while ( rs.next() ) {
+                System.out.printf(rs.getString( 1 ), rs.getString( 2 ),rs.getInt(3), rs.getString(4), rs.getInt(5), rs.getInt(6));
+            }
+
+        } catch ( SQLException e ) {
+            System.err.println( "Error during Demo!" );
+            e.printStackTrace();
+        }
+        stmt.close();
+
+        /*Gruppiert die Zahlungen nach Zahlungsart und Anzahl.*/
+        stmt = con.createStatement();
+        try ( ResultSet rs = stmt.executeQuery( "SELECT COUNT(RechnungsNr), Zahlungsart from portfolio.Zahlung Group by Zahlungsart" ) ) {
+            while ( rs.next() ) {
+                System.out.printf(rs.getString( 2 ), rs.getInt( 1 ));
+            }
+
+        } catch ( SQLException e ) {
+            System.err.println( "Error during Demo!" );
+            e.printStackTrace();
+        }
+        stmt.close();
     }
 
 }
