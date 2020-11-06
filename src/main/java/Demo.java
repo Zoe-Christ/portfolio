294eef3a75855c32f5aa6bb2b1165e7eb282bc0b
Zoe Christ <zoe@christ.de>
2020-11-06 12:28:22 +0100

    sql statements

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -24,7 +24,7 @@ public class Demo {
         try ( ResultSet rs = stmt.executeQuery( "SELECT Lieferdienst.Name, Lieferdienst.vertrag_Vertrag_ID, Vertrag.Vertrag_ID, Vertrag.Laufzeit from portfolio.Lieferdienst \n" +
                 "inner Join portfolio.Vertrag where Lieferdienst.vertrag_Vertrag_ID = Vertrag.Vertrag_ID" ) ) {
                         while ( rs.next() ) {
-                            System.out.printf(rs.getString( 1 ), rs.getInt( 2 ),rs.getInt(3), rs.getString(4) );
+                            System.out.printf(""+rs.getString( 1 ), rs.getInt( 2 ),rs.getInt(3), rs.getString(4) );
                         }
 
         } catch ( SQLException e ) {
@@ -37,7 +37,7 @@ public class Demo {
         stmt = con.createStatement();
         try ( ResultSet rs = stmt.executeQuery( "SELECT * from portfolio.Filiale order by Umsatz DESC" ) ) {
             while ( rs.next() ) {
-                System.out.printf(rs.getString( 1 ), rs.getString( 2 ),rs.getInt(3), rs.getString(4), rs.getInt(5), rs.getInt(6));
+                System.out.printf(""+rs.getInt( 1 ), rs.getString( 2 ),rs.getInt(3), rs.getString(4), rs.getInt(5), rs.getInt(6));
             }
 
         } catch ( SQLException e ) {
@@ -50,7 +50,59 @@ public class Demo {
         stmt = con.createStatement();
         try ( ResultSet rs = stmt.executeQuery( "SELECT COUNT(RechnungsNr), Zahlungsart from portfolio.Zahlung Group by Zahlungsart" ) ) {
             while ( rs.next() ) {
-                System.out.printf(rs.getString( 2 ), rs.getInt( 1 ));
+                System.out.printf(""+rs.getString( 2 ), rs.getInt( 1 ));
+            }
+
+        } catch ( SQLException e ) {
+            System.err.println( "Error during Demo!" );
+            e.printStackTrace();
+        }
+        stmt.close();
+
+        /*Sucht alle Händler, deren Namen mit 'Ama' beginnt.*/
+        stmt = con.createStatement();
+        try ( ResultSet rs = stmt.executeQuery( "SELECT * from portfolio.Online_haendler where Name LIKE 'Ama%'" ) ) {
+            while ( rs.next() ) {
+                System.out.printf(""+rs.getInt( 1 ), rs.getString( 2 ), rs.getString(3),rs.getString(4), rs.getInt(5));
+            }
+
+        } catch ( SQLException e ) {
+            System.err.println( "Error during Demo!" );
+            e.printStackTrace();
+        }
+        stmt.close();
+
+        /*Zeigt die ID und die Mietkosten aller Lager, deren Mietkosten kleiner als 9000 sind.*/
+        stmt = con.createStatement();
+        try ( ResultSet rs = stmt.executeQuery( "SELECT LagerID, Mietkosten from portfolio.Lager where Mietkosten < 9000" ) ) {
+            while ( rs.next() ) {
+                System.out.printf(""+rs.getInt( 1 ), rs.getString( 2 ));
+            }
+
+        } catch ( SQLException e ) {
+            System.err.println( "Error during Demo!" );
+            e.printStackTrace();
+        }
+        stmt.close();
+
+        /*Zeigt alle Produkte in einem Kundenauftrag, die häufiger als einmal bestellt wurden.*/
+        stmt = con.createStatement();
+        try ( ResultSet rs = stmt.executeQuery( "SELECT Name, ProduktNr from portfolio.Produkt WHERE ProduktNr IN (SELECT Produkt_ProduktNr from portfolio.Kundenauftrag_has_produkt where stueckzahl > 1)" ) ) {
+            while ( rs.next() ) {
+                System.out.printf(""+rs.getString( 1 ), rs.getInt( 2 ));
+            }
+
+        } catch ( SQLException e ) {
+            System.err.println( "Error during Demo!" );
+            e.printStackTrace();
+        }
+        stmt.close();
+
+        /*Zeigt zugestellte Bestellungen mit Warenwert zwischen 400 und 1000 € sortiert nach absteigendem Warenwert.*/
+        stmt = con.createStatement();
+        try ( ResultSet rs = stmt.executeQuery( "Select Warenwert FROM Bestellung WHERE Bearbeitungsstatus = \"zugestellt\" OR Warenwert BETWEEN 400 AND 1000 ORDER BY Warenwert DESC" ) ) {
+            while ( rs.next() ) {
+                System.out.printf(""+rs.getInt( 1 ));
             }
 
         } catch ( SQLException e ) {
